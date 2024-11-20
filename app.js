173e6a6e14a4793f1c5009f7e7495ff2dd9c93e4
Anderson Johvanny Rosario Peña <60318028+Andersonandy1812@users.noreply.github.com>
2024-11-20 12:03:44 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -6,3 +6,20 @@ function myFunction() {
       x.className = "right-list";
     }
   }
+ // Seleccionar el botón
+ const volverPrincipal = document.getElementById('volverPrincipal');
+
+ // Detectar el evento de scroll
+ window.addEventListener('scroll', () => {
+     // Mostrar el botón si el desplazamiento es mayor a 100px
+     if (window.scrollY > 100) {
+      volverPrincipal.style.display = 'block';
+     } else {
+      volverPrincipal.style.display = 'none';
+     }
+ });
+
+ // Opcional: Agregar un evento al botón (por ejemplo, para subir al inicio)
+ volverPrincipal.addEventListener('click', () => {
+     window.scrollTo({ top: 0, behavior: 'smooth' });
+ });
